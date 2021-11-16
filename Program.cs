b58ad15c3cd5c2b6d4aa4cd6f10c043c2ec0844f
Kaitlyn Long <katielong11@gmail.com>
2021-11-16 14:42:01 -0600

    completed phase 2 of Guessing Game

--- Program.cs
+++ Program.cs
@@ -7,7 +7,18 @@ void Main()
     Console.WriteLine("------------------------------");
     Console.WriteLine();
 
-    Console.Write($"{""} ");
-    string answer = Console.ReadLine().ToLower();
+    int answer = int.Parse(Console.ReadLine());
     // allows a user to input a number and displays it back on the screen
+    int secretNumber = 42;
+    // define the secret number
+    if (answer == secretNumber)
+    {
+        Console.WriteLine("Correct guess!");
+        //displays if the user selects the correct number
+    }
+    else
+    {
+        Console.WriteLine("Guess again!");
+        //displays if the user selects the incorrect number
+    }
 }
